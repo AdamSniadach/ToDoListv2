8c285721f6f1dca6b9d6af79e8704cc1824da4ab
Adam <126939161+AdamSniadach@users.noreply.github.com>
2023-05-28 17:38:56 +0100

    Starter-template

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port 3000`);
 });
